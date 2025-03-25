0f9a0c2a9a1868e2d691100c42d77350f3fa0c70
Jon Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2025-03-25 09:12:58 +0000

    Exception testing Resultfile.WithAnalysisFileName

--- tests/Hutch.Rackit.Tests/ResultFileExtensionsTests/WithAnalysisFileNameTests.cs
+++ tests/Hutch.Rackit.Tests/ResultFileExtensionsTests/WithAnalysisFileNameTests.cs
@@ -1,6 +1,27 @@
+using Hutch.Rackit.TaskApi;
+using Hutch.Rackit.TaskApi.Models;
+
 namespace Hutch.Rackit.Tests.ResultFileExtensionsTests;
 
 public class WithAnalysisFileNameTests
 {
+  private const string _garbageString = "garbage";
   
+  [Theory]
+  [InlineData("", "")]
+  [InlineData(_garbageString, "")]
+  [InlineData(AnalysisType.AnalyticsGenePhewas, "")]
+  [InlineData(AnalysisType.AnalyticsGenePhewas, _garbageString)]
+  [InlineData(AnalysisType.Distribution, "")]
+  [InlineData(AnalysisType.Distribution, _garbageString)]
+  [InlineData(AnalysisType.Distribution, DistributionCode.Icd)]
+  [InlineData(AnalysisType.AnalyticsGenePhewas, DistributionCode.Generic)]
+  public void WhenUnsupportedAnalysis_ShouldThrowNotImplemented(
+    string analysisType, string analysisCode)
+  {
+    var resultFile = new ResultFile();
+    
+    Assert.Throws<NotImplementedException>(() => 
+      resultFile.WithAnalysisFileName(analysisCode, analysisCode));
+  }
 }
