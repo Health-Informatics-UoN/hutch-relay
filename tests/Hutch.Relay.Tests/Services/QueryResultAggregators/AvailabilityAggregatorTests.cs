de32ff27df895aa1c5dda9102fa4a649612a0c0a
Jon Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2025-03-26 15:39:59 +0000

    AvailabilityAggregator unit tests

--- tests/Hutch.Relay.Tests/Services/QueryResultAggregators/AvailabilityAggregatorTests.cs
+++ tests/Hutch.Relay.Tests/Services/QueryResultAggregators/AvailabilityAggregatorTests.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Hutch.Rackit.TaskApi.Models;
 using Hutch.Relay.Models;
 using Hutch.Relay.Services;
@@ -44,5 +45,89 @@ public class AvailabilityAggregatorTests
     aggregator.Process(subTasks);
 
     obfuscator.Verify(x => x.Obfuscate(It.IsAny<int>()), Times.Once);
+  }
+
+  [Fact]
+  public void WhenOneSubTask_AggregateCountIsUnchanged()
+  {
+    const int expectedCount = 50;
+
+    var subTasks = new List<RelaySubTaskModel>()
+    {
+      new()
+      {
+        Id = Guid.NewGuid(),
+        Owner = new() { Id = Guid.NewGuid(), Owner = "test" },
+        RelayTask = new(),
+        Result = JsonSerializer.Serialize(new JobResult
+        {
+          Results = new()
+          {
+            Count = expectedCount
+          }
+        })
+      }
+    };
+
+    var expected = new QueryResult
+    {
+      Count = expectedCount,
+      Files = [],
+      DatasetCount = 0
+    };
+
+    var obfuscator = new Mock<IObfuscator>();
+    obfuscator.Setup(x => x.Obfuscate(It.IsAny<int>()))
+      .Returns(() => expectedCount);
+
+    var aggregator = new AvailabilityAggregator(obfuscator.Object);
+
+    var actual = aggregator.Process(subTasks);
+
+    Assert.Equivalent(expected, actual);
+  }
+
+  [Theory]
+  [InlineData(new[] { 3, 4, 5 })]
+  [InlineData(new[] { 1, 2, 3, 4, 5 })]
+  [InlineData(new[] { 42 })]
+  [InlineData(new[] { 70, 431, 5423652, 3214, 654, 3213, 5342, 54, 65476, 76547, 234, 2, 5532 })]
+  public void WhenSubTasks_AggregateCountIsSum(int[] subtaskCounts)
+  {
+    var expectedCount = subtaskCounts.Sum();
+
+    var subTasks = subtaskCounts
+      .Select(count =>
+        new RelaySubTaskModel
+        {
+          Id = Guid.NewGuid(),
+          Owner = new() { Id = Guid.NewGuid(), Owner = "test" },
+          RelayTask = new(),
+          Result = JsonSerializer.Serialize(new JobResult
+          {
+            Results = new()
+            {
+              Count = count
+            }
+          })
+        })
+      .ToList();
+
+    var expected = new QueryResult
+    {
+      Count = expectedCount,
+      Files = [],
+      DatasetCount = 0
+    };
+
+    var obfuscator = new Mock<IObfuscator>();
+    obfuscator.Setup(x => x.Obfuscate(It.IsAny<int>()))
+      .Returns(() => expectedCount);
+
+    var aggregator = new AvailabilityAggregator(obfuscator.Object);
+
+    var actual = aggregator.Process(subTasks);
+
+    Assert.Equivalent(expected, actual);
   }
 }
