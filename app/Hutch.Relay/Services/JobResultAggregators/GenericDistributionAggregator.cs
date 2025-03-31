15808f88d918b7da6e76c502556da9fbc79477e0
Jon Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2025-03-31 11:03:03 +0100

    Code Distribution Aggregator unit tests and fixes

--- app/Hutch.Relay/Services/JobResultAggregators/GenericDistributionAggregator.cs
+++ app/Hutch.Relay/Services/JobResultAggregators/GenericDistributionAggregator.cs
@@ -1,6 +1,7 @@
 using System.Globalization;
 using System.Text.Json;
 using CsvHelper;
+using CsvHelper.Configuration;
 using Hutch.Rackit.TaskApi;
 using Hutch.Rackit.TaskApi.Models;
 using Hutch.Relay.Models;
@@ -11,16 +12,20 @@ public class GenericDistributionAggregator(IObfuscator obfuscator) : IQueryResul
 {
   private static List<GenericDistributionRecord> ParseResultFile(string tsvData)
   {
+    var config = CsvConfiguration.FromAttributes<GenericDistributionRecord>();
+    config.MissingFieldFound = null; // The model will initialise missing fields
+
     using var reader = new StringReader(tsvData);
-    using var tsv = new CsvReader(reader, CultureInfo.InvariantCulture);
+    using var tsv = new CsvReader(reader, config);
 
     return tsv.GetRecords<GenericDistributionRecord>().ToList();
   }
 
   public QueryResult Process(List<RelaySubTaskModel> subTasks)
   {
-    // A Dictionary of aggregated results records keyed by Code
-    Dictionary<string, GenericDistributionRecord> aggregatedRecords = new();
+    // Aggregation State
+    (string collectionId, Dictionary<string, GenericDistributionRecord> aggregatedRecords) accumulator =
+      (subTasks.FirstOrDefault()?.RelayTask.Collection ?? string.Empty, new());
 
     // Aggregate across all valid subtasks
     foreach (var subTask in subTasks)
@@ -33,19 +38,28 @@ public class GenericDistributionAggregator(IObfuscator obfuscator) : IQueryResul
       // TODO: review this behaviour
       if (result is null) continue;
 
+      // If the QueryResult says there's no data, jog on
+      if (result.Results.Count == 0) continue;
+
       // Find the relevant ResultFile (if none found, this subtask will not contribute results)
       foreach (var file in result.Results.Files)
       {
         if (file.FileName != ResultFileName.CodeDistribution) continue;
+        var rawFileData = file.DecodeData();
 
-        var records = ParseResultFile(file.DecodeData());
+        // Check we have more than just the header row; CsvHelper won't parse it if there's no actual data
+        // This could happen if the QueryResult.Count was a lie ;) or just if the file was populated weirdly
+        if (rawFileData.Split("\n").Length < 2) continue;
 
-        aggregatedRecords.AccumulateData(records);
+        // If we actually have data, go ahead and parse
+        var records = ParseResultFile(rawFileData);
+
+        accumulator.AccumulateData(records);
       }
     }
 
     // Perform a final obfuscation pass of the now aggregated values as we convert them to a List for encoding
-    var finalData = aggregatedRecords
+    var finalData = accumulator.aggregatedRecords
       .Select(x =>
         x.Value with
         {
@@ -87,27 +101,27 @@ file static class GenericDistributionAggregatorExtensions
   /// <param name="records">The list of records to provide further data.</param>
   /// <returns>The updated accumulated dataset.</returns>
   public static void AccumulateData(
-    this Dictionary<string, GenericDistributionRecord> accumulator,
+    this (string collectionId, Dictionary<string, GenericDistributionRecord> records) accumulator,
     List<GenericDistributionRecord> records)
   {
     foreach (var record in records)
     {
-      if (!accumulator.ContainsKey(record.Code))
+      if (!accumulator.records.TryGetValue(record.Code, out var accumulatorRecord))
       {
         // Create a brand new "base" record, inheriting most (but not all) from the current value
-        accumulator.Add(
+        accumulator.records.Add(
           record.Code,
           record with
           {
             // Override Collection with the Upstream ID instead of Downstream
-            Collection = "none"
+            Collection = accumulator.collectionId,
           });
       }
       else
       {
         // Key was already present
         // merge Count but otherwise keep the base Accumulator record's properties
-        accumulator[record.Code].Count += record.Count;
+        accumulatorRecord.Count += record.Count;
       }
     }
   }
---
+++ tests/Hutch.Relay.Tests/Services/QueryResultAggregators/GenericDistributionAggregatorTests.cs
@@ -0,0 +1,184 @@
+using System.Text.Json;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Hutch.Rackit.TaskApi;
+using Hutch.Rackit.TaskApi.Models;
+using Hutch.Relay.Constants;
+using Hutch.Relay.Models;
+using Hutch.Relay.Services;
+using Hutch.Relay.Services.JobResultAggregators;
+using Microsoft.VisualBasic;
+using Moq;
+using Xunit;
+
+namespace Hutch.Relay.Tests.Services.QueryResultAggregators;
+
+public class GenericDistributionAggregatorTests
+{
+  // We don't do in depth obfuscation tests here; they are done in the Obfuscator test suite.
+  // But we do attempt to ensure that the Obfuscator is being applied to aggregate outputs
+
+  #region Test Data
+
+  private static RelaySubTaskModel GenerateSubTaskWithResultsData(List<GenericDistributionRecord> data)
+  {
+    var subTaskId = Guid.NewGuid();
+    var subNodeId = data.FirstOrDefault()?.Collection ?? Guid.NewGuid().ToString();
+
+    return new()
+    {
+      Id = subTaskId,
+      Owner = new() { Id = Guid.NewGuid(), Owner = "test_user" },
+      RelayTask = new()
+      {
+        Id = Guid.NewGuid().ToString(),
+        Collection = "parent_collection",
+        Type = TaskTypes.TaskApi_CodeDistribution,
+      },
+      Result = JsonSerializer.Serialize(new JobResult
+      {
+        Uuid = subTaskId.ToString(),
+        CollectionId = subNodeId,
+        Results = new()
+        {
+          Count = data.Count,
+          DatasetCount = 1,
+          Files =
+          [
+            new ResultFile()
+              .WithAnalysisFileName(AnalysisType.Distribution, DistributionCode.Generic)
+              .WithData(data)
+          ]
+        }
+      })
+    };
+  }
+
+  private static List<GenericDistributionRecord> GenerateSubTaskResults(
+    string collectionId, int[] counts, int codeOffset = 1)
+    => counts.Select((v, i) => new GenericDistributionRecord
+    {
+      Code = $"CODE{i + codeOffset}",
+      Collection = collectionId,
+      Count = v,
+    }).ToList();
+
+  public static readonly RelaySubTaskModel SubTaskNoData = GenerateSubTaskWithResultsData([]);
+
+  public static readonly RelaySubTaskModel SubTaskZeroCounts =
+    GenerateSubTaskWithResultsData(GenerateSubTaskResults(
+      Guid.NewGuid().ToString(),
+      [0, 0, 0]));
+
+  public static readonly RelaySubTaskModel SubTaskWithCounts1 =
+    GenerateSubTaskWithResultsData(GenerateSubTaskResults(
+      Guid.NewGuid().ToString(),
+      [91, 230, 1342, 17]));
+
+  public static readonly RelaySubTaskModel SubTaskWithCounts2 =
+    GenerateSubTaskWithResultsData(GenerateSubTaskResults(
+      Guid.NewGuid().ToString(),
+      [412, 0, 975, 26],
+      3));
+
+  public static IEnumerable<object[]> GetSubTasks()
+  {
+    yield return [new List<RelaySubTaskModel>(), 0, new List<int>()];
+    yield return [new List<RelaySubTaskModel> { SubTaskNoData }, 0, new List<int>()];
+    yield return [new List<RelaySubTaskModel> { SubTaskZeroCounts }, 3, new List<int> { 0, 0, 0 }];
+    yield return [new List<RelaySubTaskModel> { SubTaskWithCounts1 }, 4, new List<int> { 91, 230, 1342, 17 }];
+    yield return
+    [
+      new List<RelaySubTaskModel> { SubTaskZeroCounts, SubTaskWithCounts1, SubTaskWithCounts2 }, 6,
+      new List<int> { 91, 230, 1754, 17, 975, 26 }
+    ];
+  }
+
+  #endregion
+
+  [Fact]
+  public void WhenNoSubTasks_ReturnEmptyQueryResult()
+  {
+    var subTasks = new List<RelaySubTaskModel>();
+
+    var expected = new QueryResult
+    {
+      Count = 0,
+      Files = [],
+      DatasetCount = 0
+    };
+
+    var obfuscator = new Mock<IObfuscator>();
+
+    var aggregator = new GenericDistributionAggregator(obfuscator.Object);
+
+    var actual = aggregator.Process(subTasks);
+
+    Assert.Equivalent(expected, actual);
+  }
+
+  [Theory]
+  [MemberData(nameof(GetSubTasks))]
+  public void ObfuscatorIsCalledOncePerAggregatedFileRow(List<RelaySubTaskModel> subTasks, int aggregatedRowCount,
+    List<int> expectedAggregates)
+  {
+    var obfuscator = new Mock<IObfuscator>();
+
+    var aggregator = new GenericDistributionAggregator(obfuscator.Object);
+    aggregator.Process(subTasks);
+
+    obfuscator.Verify(x => x.Obfuscate(It.IsAny<int>()), Times.Exactly(aggregatedRowCount));
+  }
+
+  [Theory]
+  [MemberData(nameof(GetSubTasks))]
+  public void ReturnsCorrectPropertiesAndAggregates(List<RelaySubTaskModel> subTasks,
+    int aggregatedRowCount,
+    List<int> expectedAggregates)
+  {
+    var obfuscator = new Mock<IObfuscator>();
+    obfuscator.Setup(x => x.Obfuscate(It.IsAny<int>()))
+      .Returns((int value) => value);
+
+    var aggregator = new GenericDistributionAggregator(obfuscator.Object);
+
+    var actual = aggregator.Process(subTasks);
+
+    // Check the count fields
+    Assert.Equal(aggregatedRowCount, actual.Count);
+    if (aggregatedRowCount + expectedAggregates.Count == 0)
+    {
+      // If zero rows, should be no files
+      Assert.Equal(0, actual.DatasetCount);
+      Assert.Empty(actual.Files);
+      return;
+    }
+
+    // With results there should only be 1 file
+    Assert.Equal(1, actual.DatasetCount);
+    Assert.Single(actual.Files);
+
+    // If we have results, parse the result ourselves for assertion
+    var decodedFileResult = actual.Files.Single().DecodeData();
+    var config = CsvConfiguration.FromAttributes<GenericDistributionRecord>();
+    config.MissingFieldFound = null;
+    using var reader = new StringReader(decodedFileResult);
+    using var csv = new CsvReader(reader, config);
+    var rowsByCode = csv.GetRecords<GenericDistributionRecord>()
+      .ToDictionary(x => x.Code, x => (aggregate: x.Count, collection: x.Collection));
+
+    // Check the row count matches what's expected and what's described
+    Assert.Equal(expectedAggregates.Count, rowsByCode.Count);
+    Assert.Equal(actual.Count, rowsByCode.Count);
+
+    // Check each row's count and collection
+    for (var i = 0; i < expectedAggregates.Count; i++)
+    {
+      var expected = expectedAggregates[i];
+      var code = $"CODE{i + 1}";
+
+      Assert.Equal(expected, rowsByCode[code].aggregate);
+      Assert.Equal(subTasks.First().RelayTask.Collection, rowsByCode[code].collection);
+    }
+  }
+}
