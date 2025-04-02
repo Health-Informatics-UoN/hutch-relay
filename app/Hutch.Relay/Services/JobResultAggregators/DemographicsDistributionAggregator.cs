cc77d74c38f1520dc858d59bcf9516d5b49901ce
Jon Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2025-04-02 14:07:17 +0100

    demographics distribution aggregator + tests so far

---
+++ app/Hutch.Relay/Services/JobResultAggregators/DemographicsDistributionAggregator.cs
@@ -0,0 +1,70 @@
+using Hutch.Rackit.TaskApi.Models;
+using Hutch.Relay.Models;
+
+namespace Hutch.Relay.Services.JobResultAggregators;
+
+public class DemographicsDistributionAggregator : IQueryResultAggregator
+{
+  public QueryResult Process(List<RelaySubTaskModel> subTasks)
+  {
+    throw new NotImplementedException();
+  }
+}
+
+internal record AlternativesAccumulator(DemographicsDistributionRecord BaseRecord)
+{
+  public Dictionary<string, List<int>> Alternatives { get; init; } = [];
+}
+
+internal record DemographicsAccumulator(string CollectionId)
+{
+  public Dictionary<string, AlternativesAccumulator> Alternatives { get; init; } = [];
+}
+
+internal static class DemographicsDistributionAggregatorExtensions
+{
+  // TODO: More work will be required to aggregate some other demographics such as AGE
+  // but this requires decisions on e.g. how to obfuscate those results.
+  // The reality is Bunny does not do these types today, so neither does Relay.
+  // Should Relay exclude these types today to avoid misrepresentation if it were provided them?
+  // Do we know exhaustively the demographics codes we can/can't do?
+  public static DemographicsAccumulator AccumulateData(
+    this DemographicsAccumulator accumulator,
+    List<DemographicsDistributionRecord> records)
+  {
+    foreach (var record in records)
+    {
+      if (accumulator.Alternatives.TryGetValue(record.Code, out var alternativesAccumulator))
+      {
+        // Key was already present
+
+        // TODO: Different Logic for Age goes here? for now ignore Age demographics
+        if (record.Code == "AGE") continue;
+
+        // Add Alternatives
+        var alternatives = record.GetAlternatives();
+
+        foreach (var (alternative, value) in alternatives)
+        {
+          if (!alternativesAccumulator.Alternatives.ContainsKey(alternative))
+            alternativesAccumulator.Alternatives[alternative] = [];
+          alternativesAccumulator.Alternatives[alternative].Add(value);
+        }
+      }
+      else
+      {
+        // Create a brand new coded entry, with a base record we can use later to populate final properties
+        accumulator.Alternatives.Add(
+          record.Code,
+          new(record)
+          {
+            Alternatives = record.GetAlternatives()
+              .Select(x => (x.Key, new List<int> { x.Value }))
+              .ToDictionary()
+          });
+      }
+    }
+
+    return accumulator;
+  }
+}
---
+++ tests/Hutch.Relay.Tests/Services/QueryResultAggregators/DemographicsDistributionAggregatorTests.cs
@@ -0,0 +1,168 @@
+using Hutch.Rackit.TaskApi.Models;
+using Hutch.Relay.Models;
+using Hutch.Relay.Services.JobResultAggregators;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Xunit;
+
+namespace Hutch.Relay.Tests.Services.QueryResultAggregators;
+
+public class DemographicsDistributionAggregatorTests
+{
+  // Later testing
+  //   - Test that the validated accumulator correctly finalises into expected outputs for a given input
+  //      - Test the Finaliser method(s)
+  //      - Test `Process()` end-to-end much like CodeDistribution does today
+
+  #region AccumulateData
+
+  private readonly DemographicsAccumulator _singleCodeAccumulator =
+    new("test_collection")
+    {
+      Alternatives =
+      {
+        ["EXISTING_CODE"] = new(new()
+        {
+          Collection = "sub_collection",
+          Code = "EXISTING_CODE",
+          Description = "TEST_DESCRIPTION",
+          Alternatives = "^BRACKET1|50^BRACKET2|70^"
+        })
+        {
+          Alternatives =
+          {
+            ["BRACKET1"] = [50],
+            ["BRACKET2"] = [70]
+          }
+        }
+      }
+    };
+
+  [Fact]
+  public void AccumulateData_WhenFirstRowForCode_CorrectAlternativesAccumulation()
+  {
+    // When a code isn't already in the accumulated state:
+    //   - base record is set to the record in question
+    //   - alternatives are initialised with values from the record in question
+
+    var initialAccumulator = _singleCodeAccumulator;
+
+    List<DemographicsDistributionRecord> inputRecords =
+    [
+      new() { Code = "SEX", Collection = "sub_collection2", Alternatives = "^MALE|155^FEMALE|135^" }
+    ];
+
+    // Much of our expected state has to be redefined manually due to mutable reference types
+    var expectedSexBaseRecord = inputRecords[0];
+    Dictionary<string, List<int>> expectedSexAlternatives = new() { ["MALE"] = [155], ["FEMALE"] = [135] };
+    Dictionary<string, List<int>> expectedExistingAlternatives = new() { ["BRACKET1"] = [50], ["BRACKET2"] = [70] };
+    DemographicsDistributionRecord expectedExistingBaseRecord = new()
+    {
+      Collection = "sub_collection",
+      Code = "EXISTING_CODE",
+      Description = "TEST_DESCRIPTION",
+      Alternatives = "^BRACKET1|50^BRACKET2|70^"
+    };
+
+    var actual = initialAccumulator.AccumulateData(inputRecords);
+
+    // Here's what we added
+    Assert.Equivalent(
+      new AlternativesAccumulator(expectedSexBaseRecord) { Alternatives = expectedSexAlternatives },
+      actual.Alternatives["SEX"]);
+
+    // Here's what should remain unchanged
+    Assert.Equivalent(
+      new AlternativesAccumulator(expectedExistingBaseRecord) { Alternatives = expectedExistingAlternatives },
+      actual.Alternatives["EXISTING_CODE"]);
+  }
+
+  [Fact]
+  public void AccumulateData_WhenNewRowForExistingCode_CorrectAlternativesAccumulation()
+  {
+    // When a code is already present
+    //   - base record remains as previous
+    //   - alternatives are added to existing alternative keys
+    //   - new alternatives keys are initialised // Not sure if this is technically legal in Task API but err on flexibility
+
+    var initialAccumulator = _singleCodeAccumulator;
+
+    List<DemographicsDistributionRecord> inputRecords =
+    [
+      new()
+      {
+        Code = "EXISTING_CODE", Collection = "sub_collection2",
+        Alternatives = "^BRACKET2|155^BRACKET1|135^BRACKET3|504^"
+      }
+    ];
+
+    // Much of our expected state has to be redefined manually due to mutable reference types
+    Dictionary<string, List<int>> expectedAlternatives =
+      new() { ["BRACKET1"] = [50, 135], ["BRACKET2"] = [70, 155], ["BRACKET3"] = [504] };
+    DemographicsDistributionRecord expectedExistingBaseRecord = new()
+    {
+      Collection = "sub_collection",
+      Code = "EXISTING_CODE",
+      Description = "TEST_DESCRIPTION",
+      Alternatives = "^BRACKET1|50^BRACKET2|70^"
+    };
+
+    var actual = initialAccumulator.AccumulateData(inputRecords);
+
+    Assert.Equivalent(
+      new AlternativesAccumulator(expectedExistingBaseRecord) { Alternatives = expectedAlternatives },
+      actual.Alternatives["EXISTING_CODE"]);
+  }
+
+  [Fact]
+  public void AccumulateData_WhenDuplicateCodeRows_CorrectAlternativesAccumulation()
+  {
+    // Duplicate Codes in the same Results File
+    //   - are handled according to the same rules* - should be impossible but do it right anyway
+    // * see `AccumulateData_WhenNewRowForExistingCode_CorrectAlternativesAccumulation`
+    //    and `AccumulateData_WhenFirstRowForCode_CorrectAlternativesAccumulation`
+
+    var initialAccumulator = _singleCodeAccumulator;
+
+    List<DemographicsDistributionRecord> inputRecords =
+    [
+      new() { Code = "EXISTING_CODE", Collection = "sub_collection2", Alternatives = "^BRACKET2|155^BRACKET1|135^" },
+      new() { Code = "EXISTING_CODE", Collection = "sub_collection2", Alternatives = "^BRACKET1|27^BRACKET2|32^" },
+      new() { Code = "SEX", Collection = "sub_collection2", Alternatives = "^MALE|155^FEMALE|135^" },
+      new() { Code = "SEX", Collection = "sub_collection2", Alternatives = "^MALE|42^FEMALE|68^OTHER|7^" }
+    ];
+
+    // Much of our expected state has to be redefined manually due to mutable reference types
+    Dictionary<string, List<int>> expectedExistingAlternatives =
+      new() { ["BRACKET1"] = [50, 135, 27], ["BRACKET2"] = [70, 155, 32] };
+    Dictionary<string, List<int>> expectedSexAlternatives =
+      new() { ["MALE"] = [155, 42], ["FEMALE"] = [135, 68], ["OTHER"] = [7] };
+
+    var actual = initialAccumulator.AccumulateData(inputRecords);
+
+    // New Code
+    Assert.Equivalent(
+      expectedSexAlternatives,
+      actual.Alternatives["SEX"].Alternatives);
+
+    // Existing Code
+    Assert.Equivalent(
+      expectedExistingAlternatives,
+      actual.Alternatives["EXISTING_CODE"].Alternatives);
+  }
+
+
+  public void AccumulateData_WhenResultsIncludeAgeRecords_CorrectAlternativesAccumulation(bool ageOnly)
+  {
+    DemographicsAccumulator initialAccumulator = new("test_collection");
+
+    DemographicsDistributionRecord ageRecord = new()
+      { Collection = "sub_collection", Code = "AGE", Count = 16, Min = 9, Max = 25, Mean = 16.75, Median = 15.5 };
+  }
+
+  public void AccumulateData_WhenResultsIncludeAgeRecords_AgeRecordsAreIgnored(bool ageOnly)
+  {
+    DemographicsAccumulator initialAccumulator = new("test_collection");
+  }
+
+  #endregion
+}
