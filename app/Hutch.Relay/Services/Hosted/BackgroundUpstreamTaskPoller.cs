5e32d4f9a896c4fee963291d587dc862508572c5
Jon Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2025-05-28 08:36:47 +0000

    Background Poller now pro-actively checks EnsureSubNodes on a unique context before polling

--- app/Hutch.Relay/Services/Hosted/BackgroundUpstreamTaskPoller.cs
+++ app/Hutch.Relay/Services/Hosted/BackgroundUpstreamTaskPoller.cs
@@ -10,7 +10,16 @@ public class BackgroundUpstreamTaskPoller(
   {
     while (!stoppingToken.IsCancellationRequested)
     {
-      using var scope = serviceScopeFactory.CreateScope(); // TODO: We need more granular scope for EF, e.g. each polling thread? each job handling thread?
+      // use a shortlived scope to run some db checks without keeping a context open
+      using(var initScope = serviceScopeFactory.CreateScope()) {
+        var subnodes = initScope.ServiceProvider.GetRequiredService<SubNodeService>();
+        
+        // Ensure we have subnodes before we start polling; this is considered critical
+        subnodes.EnsureSubNodes();
+      }
+
+      // use a longer-lived scope to run the poller and its threads
+      using var scope = serviceScopeFactory.CreateScope();
       var poller = scope.ServiceProvider.GetRequiredService<UpstreamTaskPoller>();
 
       await poller.PollAllQueues(stoppingToken);
