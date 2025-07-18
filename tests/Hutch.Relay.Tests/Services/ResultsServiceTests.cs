70956de63f4f3ef1312d8ece9d93863cc975e280
Jon Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2025-07-18 08:35:58 +0000

    ResultsService tests for enabling/disabling Upstream Task API

--- tests/Hutch.Relay.Tests/Services/ResultsServiceTests.cs
+++ tests/Hutch.Relay.Tests/Services/ResultsServiceTests.cs
@@ -1,4 +1,6 @@
+using Castle.Core.Logging;
 using Hutch.Rackit;
+using Hutch.Rackit.TaskApi.Contracts;
 using Hutch.Rackit.TaskApi.Models;
 using Hutch.Relay.Config;
 using Hutch.Relay.Constants;
@@ -6,6 +8,7 @@ using Hutch.Relay.Models;
 using Hutch.Relay.Services;
 using Hutch.Relay.Services.Contracts;
 using Hutch.Relay.Services.JobResultAggregators;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
 using Xunit;
@@ -14,6 +17,63 @@ namespace Hutch.Relay.Tests.Services;
 
 public class ResultsServiceTests
 {
+  [Theory]
+  [InlineData(true)]
+  [InlineData(false)]
+  public async Task CompleteRelayTask_SubmitsResults_WhenUpstreamTaskApiIsEnabled(bool upstreamTaskApiIsEnabled)
+  {
+    var relayTask = new RelayTaskModel()
+    {
+      Id = Guid.NewGuid().ToString(),
+      Collection = Guid.NewGuid().ToString(),
+      Type = TaskTypes.TaskApi_Availability,
+    };
+
+    var tasks = new Mock<IRelayTaskService>();
+    tasks.Setup(x =>
+        x.ListSubTasks(
+          It.Is<string>(y => y == relayTask.Id),
+          It.Is<bool>(y => y == true)))
+      .Returns(() => Task.FromResult<IEnumerable<RelaySubTaskModel>>([]));
+
+    var aggregator = new Mock<IQueryResultAggregator>();
+    aggregator
+      .Setup(x =>
+        x.Process(It.Is<string>(x => x == relayTask.Collection), It.IsAny<List<RelaySubTaskModel>>()))
+      .Returns(() => new() { Count = 0 });
+
+    var logger = Mock.Of<ILogger<ResultsService>>();
+
+    var taskApi = new Mock<ITaskApiClient>();
+    taskApi
+      .Setup(x =>
+        x.SubmitResultAsync(It.IsAny<string>(), It.IsAny<JobResult>(), It.IsAny<ApiClientOptions>()));
+
+    TaskApiPollingOptions taskApiOptions = new()
+    {
+      Enable = upstreamTaskApiIsEnabled
+    };
+
+    var resultsService = new ResultsService(
+      logger,
+      Options.Create(taskApiOptions),
+      Options.Create<DatabaseOptions>(new()),
+      taskApi.Object,
+      tasks.Object,
+      aggregator.Object,
+      aggregator.Object,
+      aggregator.Object
+    );
+
+    // Act
+    await resultsService.CompleteRelayTask(relayTask);
+
+    // Assert
+    taskApi.Verify(x =>
+      x.SubmitResultAsync(It.IsAny<string>(), It.IsAny<JobResult>(), It.IsAny<ApiClientOptions>()),
+      upstreamTaskApiIsEnabled ? Times.Once : Times.Never);
+  }
+
   [Fact]
   public async Task PrepareFinalJobResult_SetsCorrectBaseProperties()
   {
