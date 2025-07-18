536ff1bc9faaaac7a91d59f86a27423bfefddf0e
Jon Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2025-07-18 11:26:43 +0000

    add filtering term entity and model, and varcat mapping lookup

---
+++ app/Hutch.Relay/Models/Beacon/FilteringTerm.cs
@@ -0,0 +1,21 @@
+namespace Hutch.Relay.Models.Beacon;
+
+public class FilteringTerm
+{
+
+  /// <summary>
+  /// <para>The type of term.</para>
+  /// <para>In practice we only support `ontologyTerm` here today due to the terms source being downstream Task API Code Distribution</para>
+  /// </summary>
+  public required string Type { get; set; } = "ontologyTerm";
+
+  /// <summary>
+  /// The actual filtering term, e.g. a prefixed OMOP code: `OMOP:8507`
+  /// </summary>
+  public required string Id { get; set; }
+
+  /// <summary>
+  /// Optional friendly description or label, e.g. the full description of an OMOP term
+  /// </summary>
+  public string? Label { get; set; }
+}
