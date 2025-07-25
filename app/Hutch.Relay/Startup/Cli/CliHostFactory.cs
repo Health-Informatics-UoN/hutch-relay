b2493dc447aeb5d0880dd3be2be16fa59eefc259
Jon Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2025-07-25 21:41:55 +0000

    tidy up

--- app/Hutch.Relay/Startup/Cli/CliHostFactory.cs
+++ app/Hutch.Relay/Startup/Cli/CliHostFactory.cs
@@ -1,28 +1,5 @@
 using System.CommandLine;
 
-
-// Hosted CLI steps
-
-  // 1. Parse Root (args) -> (this is all System.Commandline)
-  // 2. Build Host (args, parseResult?, hostFactory?) -> CliApplication.Create / CliApplication.CreateBuilder / HostFactory
-  // 3. Resolve Deferred Actions (host, parseResult) -> manual or (1/2)3/4 with CliApplication.Invoke
-  // 4. Invoke ParseResult -> manual or (1/2)3/4 with CliApplication.Invoke
-
-  // Helpers -> x
-  // Invoke(args, root, hostFactory) -> Does all steps, allows hostFactory to customise host building
-  // Invoke(parseResult, host) -> Does Deferred Action resolution and Invokes the result only, allows prebuilt host to customise hot building (but requires pre-parsing)
-
-  // DeferredAction handling
-  // Provide and wire up event handlers for deferred actions
-  // CliApplication COULD expose an API for replacing the built-in handlers for experienced users?
-  //   Would have to make the default handlers available if you were using Invoke so you oculd revert
-  //   But you could use DeferredActions without CliApplication.Invoke if you didn't need the base handlers
-  //   Or add a "handled" status to EventArgs so that the base handlers would run if not handled
-  //   Usage without Invoke is still weird though since that does the wiring
-  // Manual handling could be written to do different things (e.g. bootstrap a different host) for different DeferredAction<T>, depending on the child Action
-  //   This deferring would enable easy host creation (e.g. use of args) but not require the same everytime.
-  //   I'd expect users to write this themselves, possibly using Invoke() as an example
-
 namespace Hutch.Relay.Startup.Cli;
 
 public static class CliHostFactory
