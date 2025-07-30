6c1d1b56d9df4066f5ec83c2670fc285d141b525
Jon Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2025-07-30 09:16:56 +0000

    CacheUpdatedTerms implementation and tests; switch to Sqlite in memory testing db

--- tests/Hutch.Relay.Tests/Services/FilteringTermsServiceTests.cs
+++ tests/Hutch.Relay.Tests/Services/FilteringTermsServiceTests.cs
@@ -1,7 +1,9 @@
+using System.Data.Common;
 using Hutch.Rackit.TaskApi;
 using Hutch.Rackit.TaskApi.Models;
 using Hutch.Relay.Config.Beacon;
 using Hutch.Relay.Constants;
+using Hutch.Relay.Data;
 using Hutch.Relay.Data.Entities;
 using Hutch.Relay.Models;
 using Hutch.Relay.Services;
@@ -13,12 +15,29 @@ using Xunit;
 
 namespace Hutch.Relay.Tests.Services;
 
-public class FilteringTermsServiceTests
+public class FilteringTermsServiceTests : IDisposable
 {
+  private readonly DbConnection? _connection = null;
+
   private static readonly IOptions<RelayBeaconOptions> DefaultOptions = Options.Create<RelayBeaconOptions>(new()
   {
     Enable = true
   });
+
+  private readonly ApplicationDbContext _dbContext;
+
+  public FilteringTermsServiceTests()
+  {
+    // Ensure a unique DB per Test
+    _dbContext = FixtureHelpers.NewDbContext(ref _connection);
+    _dbContext.Database.EnsureCreated();
+  }
+
+  public void Dispose()
+  {
+    _dbContext.Database.EnsureDeleted();
+    _connection?.Dispose();
+  }
 
   [Theory]
   [InlineData(true)]
@@ -43,7 +62,7 @@ public class FilteringTermsServiceTests
       Options.Create<RelayBeaconOptions>(new()
       {
         Enable = isBeaconEnabled
-      }), subNodes.Object, downstreamTasks.Object);
+      }), subNodes.Object, downstreamTasks.Object, _dbContext);
 
     await filteringTermsService.RequestUpdatedTerms();
 
@@ -52,7 +71,7 @@ public class FilteringTermsServiceTests
         LogLevel.Warning, // Match whichever log level you want here
         0, // EventId
         It.Is<It.IsAnyType>((o, t) => string.Equals(
-          "GA4GH Beacon Functionality is disabled; not requesting FilteringTerms.", o.ToString())), // The type here must match the `logger.Log<T>` type used above
+          "GA4GH Beacon Functionality is disabled; not requesting updated Filtering Terms.", o.ToString())), // The type here must match the `logger.Log<T>` type used above
         null, //It.IsAny<Exception>(), // Whatever exception may have been logged with it, change as needed.
         (Func<It.IsAnyType, Exception?, string>)It.IsAny<object>()), // The message formatter
       isBeaconEnabled ? Times.Never : Times.Once);
@@ -91,7 +110,7 @@ public class FilteringTermsServiceTests
     var downstreamTasks = new Mock<IDownstreamTaskService>();
     downstreamTasks.Setup(x => x.Enqueue(It.IsAny<AvailabilityJob>(), It.IsAny<List<SubNodeModel>>()));
 
-    var service = new FilteringTermsService(logger, DefaultOptions, subNodes.Object, downstreamTasks.Object);
+    var service = new FilteringTermsService(logger, DefaultOptions, subNodes.Object, downstreamTasks.Object, _dbContext);
 
     // Act
     await service.RequestUpdatedTerms();
@@ -240,5 +259,186 @@ public class FilteringTermsServiceTests
       x => Assert.Equivalent(expected[0], x),
       x => Assert.Equivalent(expected[1], x),
       x => Assert.Equivalent(expected[2], x));
+  }
+
+  [Fact]
+  public async Task CacheUpdatedTerms_WhenNoDistributionResults_LogsWarningAndReturns()
+  {
+    // Arrange
+    var results = new JobResult()
+    {
+      Results = new QueryResult()
+      {
+        Files = [ new ResultFile()
+        {
+          FileName = ResultFileName.DemographicsDistribution,
+        }]
+      }
+    };
+
+    var logger = new Mock<ILogger<FilteringTermsService>>();
+
+    var subNodes = Mock.Of<ISubNodeService>();
+
+    var downstreamTasks = Mock.Of<IDownstreamTaskService>();
+
+    var filteringTermsService = new FilteringTermsService(
+      logger.Object,
+      Options.Create<RelayBeaconOptions>(new()
+      {
+        Enable = true
+      }), subNodes, downstreamTasks, _dbContext);
+
+    // Act
+    await filteringTermsService.CacheUpdatedTerms(results);
+
+    // Assert
+    logger.Verify(
+      x => x.Log<It.IsAnyType>( // Must use logger.Log<It.IsAnyType> to sub-out FormattedLogValues, the internal class
+        LogLevel.Warning, // Match whichever log level you want here
+        0, // EventId
+        It.Is<It.IsAnyType>((o, t) => string.Equals(
+          "A FilteringTerms update was attempted from a downstream result that contains no Code Distribution Results. The update will not proceed.", o.ToString())), // The type here must match the `logger.Log<T>` type used above
+        null, //It.IsAny<Exception>(), // Whatever exception may have been logged with it, change as needed.
+        (Func<It.IsAnyType, Exception?, string>)It.IsAny<object>()), // The message formatter
+      Times.Once);
+
+    // Nothing was done to the DB
+    Assert.Empty(_dbContext.FilteringTerms);
+  }
+
+  [Fact]
+  public async Task CacheUpdatedTerms_WhenEmptyDistributionResults_LogsWarningAndReturns()
+  {
+    // Arrange
+    var results = new JobResult()
+    {
+      Results = new QueryResult()
+      {
+        Files = [ new ResultFile()
+        {
+          FileName = ResultFileName.CodeDistribution,
+        }.WithData("HEADERS,ONLY,NO,RECORDS")]
+      }
+    };
+
+    var logger = new Mock<ILogger<FilteringTermsService>>();
+
+    var subNodes = Mock.Of<ISubNodeService>();
+
+    var downstreamTasks = Mock.Of<IDownstreamTaskService>();
+
+    var filteringTermsService = new FilteringTermsService(
+      logger.Object,
+      Options.Create<RelayBeaconOptions>(new()
+      {
+        Enable = true
+      }), subNodes, downstreamTasks, _dbContext);
+
+    // Act
+    await filteringTermsService.CacheUpdatedTerms(results);
+
+    // Assert
+    logger.Verify(
+      x => x.Log<It.IsAnyType>( // Must use logger.Log<It.IsAnyType> to sub-out FormattedLogValues, the internal class
+        LogLevel.Warning, // Match whichever log level you want here
+        0, // EventId
+        It.Is<It.IsAnyType>((o, t) => string.Equals(
+          "A FilteringTerms update was attempted from a downstream result that contains no Code Distribution Results. The update will not proceed.", o.ToString())), // The type here must match the `logger.Log<T>` type used above
+        null, //It.IsAny<Exception>(), // Whatever exception may have been logged with it, change as needed.
+        (Func<It.IsAnyType, Exception?, string>)It.IsAny<object>()), // The message formatter
+      Times.Once);
+
+    // Nothing was done to the DB
+    Assert.Empty(_dbContext.FilteringTerms);
+  }
+
+  [Fact]
+  public async Task CacheUpdatedTerms_WhenDistributionResults_ReplacesData()
+  {
+    // Arrange
+    List<FilteringTerm> oldData = [
+      new() {
+        Term = "OMOP:123",
+        SourceCategory = CodeCategory.Observation
+      },
+      new() {
+        Term = "OMOP:456",
+        SourceCategory = CodeCategory.Measurement
+      }
+    ];
+    _dbContext.AddRange(oldData);
+    await _dbContext.SaveChangesAsync();
+
+
+    var results = new JobResult()
+    {
+      Results = new QueryResult()
+      {
+        Files = [ new ResultFile()
+        {
+          FileName = ResultFileName.CodeDistribution,
+        }
+        .WithData(new List<GenericDistributionRecord>() {
+          new() {
+            Code = "OMOP:987",
+            Collection = "",
+            Category = "Gender",
+            OmopDescription = "Male"
+          },
+          new() {
+            Code = "OMOP:654",
+            Collection = "",
+            Category = "Condition",
+            OmopDescription = "Cancer"
+          },
+          new() {
+            Code = "OMOP:321",
+            Collection = "",
+            Category = "Race",
+            OmopDescription = "Human"
+          }
+        })]
+      }
+    };
+
+    List<FilteringTerm> expected = [
+      new() {
+        Term = "OMOP:987",
+        SourceCategory = "Gender",
+        Description = "Male",
+        VarCat = "person"
+      },
+      new() {
+        Term = "OMOP:654",
+        SourceCategory = "Condition",
+        Description = "Cancer",
+      },
+      new() {
+        Term = "OMOP:321",
+        SourceCategory = "Race",
+        Description = "Human",
+        VarCat = "person"
+      }
+    ];
+
+    var logger = new Mock<ILogger<FilteringTermsService>>();
+
+    var subNodes = Mock.Of<ISubNodeService>();
+
+    var downstreamTasks = Mock.Of<IDownstreamTaskService>();
+
+    var filteringTermsService = new FilteringTermsService(
+      logger.Object,
+      Options.Create<RelayBeaconOptions>(new()
+      {
+        Enable = true
+      }), subNodes, downstreamTasks, _dbContext);
+
+    // Act
+    await filteringTermsService.CacheUpdatedTerms(results);
+
+    // Assert
+    Assert.Equivalent(expected, _dbContext.FilteringTerms);
   }
 }
