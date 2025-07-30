e51f9dc57e2340443210028c62ae121bb7d22325
Jon Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2025-07-30 15:12:51 +0000

    filtering_terms endpoint

--- app/Hutch.Relay/Constants/RelayBeaconConstants.cs
+++ app/Hutch.Relay/Constants/RelayBeaconConstants.cs
@@ -1,9 +1,17 @@
 namespace Hutch.Relay.Constants;
 
+public static class BeaconApiConstants
+{
+  public const string RoutePrefix = "ga4gh/beacon/v2";
+
+  public const string UpdateTermsHeader = "X-Relay-Beacon-UpdateTerms";
+
+  public const string ForceUpdateTermsValue = "force";
+}
+
 public static class RelayBeaconTaskDetails
 {
   public const string IdPrefix = "__RELAY_BEACON__";
   public const string Collection = "__RELAY_BEACON__";
-
   public const string Owner = "__RELAY_BEACON__";
 }
--- app/Hutch.Relay/Controllers/Beacon/FilteringTermsController.cs
+++ app/Hutch.Relay/Controllers/Beacon/FilteringTermsController.cs
@@ -1,10 +1,34 @@
+using Hutch.Relay.Constants;
+using Hutch.Relay.Models.Beacon;
+using Hutch.Relay.Services.Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hutch.Relay.Controllers.Beacon;
 
 [ApiController]
-[Route("/ga4gh/beacon/v2/filtering_terms")]
-public class FilteringTermsController() : ControllerBase
+[Route($"{BeaconApiConstants.RoutePrefix}/filtering_terms")]
+public class FilteringTermsController(IFilteringTermsService filteringTerms) : ControllerBase
 {
-  
+  [HttpGet]
+  public async Task<List<FilteringTerm>> List(int skip = 0, int limit = 10)
+  {
+    // Check for custom header to queue update request
+    var hasDownstreamUpdateHeader = Request.Headers.TryGetValue(
+      BeaconApiConstants.UpdateTermsHeader,
+      out var downstreamUpdateHeader);
+
+    var mustForceDownstreamUpdate = hasDownstreamUpdateHeader &&
+      string.Equals(
+        downstreamUpdateHeader, BeaconApiConstants.ForceUpdateTermsValue,
+        StringComparison.InvariantCultureIgnoreCase);
+
+    // Fetch cached terms
+    var terms = await filteringTerms.List(skip, limit);
+
+    // Request terms update if applicable
+    if (hasDownstreamUpdateHeader || terms.Count == 0)
+      await filteringTerms.RequestUpdatedTerms(mustForceDownstreamUpdate);
+    
+    return terms;
+  }
 }
