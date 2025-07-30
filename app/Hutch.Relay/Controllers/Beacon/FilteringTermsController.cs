2fb96ca4b697c80b5604c78f4bef41ec1b87c4d7
Jon Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2025-07-30 13:37:49 +0000

    add FilteringTermsController

---
+++ app/Hutch.Relay/Controllers/Beacon/FilteringTermsController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hutch.Relay.Controllers.Beacon;
+
+[ApiController]
+[Route("/ga4gh/beacon/v2/filtering_terms")]
+public class FilteringTermsController() : ControllerBase
+{
+  
+}
