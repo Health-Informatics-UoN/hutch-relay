0947db8f1087e99005f734a88dcd2d7d6dc6c604
Jon Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2025-08-21 10:03:52 +0100

    moved auth tests to use sqlite dbcontext in the ficxture, to match other db tests

--- tests/Hutch.Relay.Tests/Auth/UserCollectionsFixture.cs
+++ tests/Hutch.Relay.Tests/Auth/UserCollectionsFixture.cs
@@ -1,3 +1,4 @@
+using System.Data.Common;
 using Hutch.Relay.Data;
 using Hutch.Relay.Data.Entities;
 using Microsoft.AspNetCore.Identity;
@@ -6,38 +7,16 @@ using Moq;
 
 namespace Hutch.Relay.Tests.Auth;
 
-public class UserCollectionsFixture
+public class UserCollectionsFixture : IDisposable
 {
-  public readonly ApplicationDbContext Database;
-
-  public static Guid SubNode1 = Guid.NewGuid();
-  public static Guid SubNode2 = Guid.NewGuid();
-
-  public static (string username, string password) User1 = ("user1", "password1");
-  public static (string username, string password) User2 = ("user2", "password2");
-
-  public static IEnumerable<object[]> GetUserCollections()
-  {
-    yield return
-    [
-      User1.username, User1.password,
-      SubNode1
-    ];
-    yield return
-    [
-      User2.username, User2.password,
-      SubNode2
-    ];
-  }
+  private readonly DbConnection? _connection = null;
+  public readonly ApplicationDbContext DbContext;
 
   public UserCollectionsFixture()
   {
-    // Set up a DB Context
-    var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-      .UseInMemoryDatabase(databaseName: "UserCollectionsTestDatabase")
-      .Options;
-
-    Database = new(options);
+    // Ensure a unique DB per Test (does it, in a fixture?)
+    DbContext = FixtureHelpers.NewDbContext(ref _connection);
+    DbContext.Database.EnsureCreated();
 
     // Populate with some test data we can use for authorisation testing
     var hasher = new PasswordHasher<RelayUser>();
@@ -60,9 +39,7 @@ public class UserCollectionsFixture
     users[0].PasswordHash = hasher.HashPassword(users[0], User1.password);
     users[1].PasswordHash = hasher.HashPassword(users[1], User2.password);
 
-    // Database.RelayUsers.AddRange(users);
-
-    Database.SubNodes.AddRange(
+    DbContext.SubNodes.AddRange(
       new()
       {
         Id = SubNode1,
@@ -74,6 +51,32 @@ public class UserCollectionsFixture
         RelayUsers = [users[1]]
       });
 
-    Database.SaveChanges();
+    DbContext.SaveChanges();
+  }
+
+  public void Dispose()
+  {
+    DbContext.Database.EnsureDeleted();
+    _connection?.Dispose();
+  }
+
+  public static Guid SubNode1 = Guid.NewGuid();
+  public static Guid SubNode2 = Guid.NewGuid();
+
+  public static (string username, string password) User1 = ("user1", "password1");
+  public static (string username, string password) User2 = ("user2", "password2");
+
+  public static IEnumerable<object[]> GetUserCollections()
+  {
+    yield return
+    [
+      User1.username, User1.password,
+      SubNode1
+    ];
+    yield return
+    [
+      User2.username, User2.password,
+      SubNode2
+    ];
   }
 }
